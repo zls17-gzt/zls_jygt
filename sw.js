737b1c7e7137a626b6a7082cfc9dc3bed01e9f3b
赵老师 <jiayuan@example.com>
2026-09-02 15:22:41 +0800

    v35: 点名册表格手机端适配，缩小列宽支持横向滚动

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'jiayuan-v34'
+const CACHE_NAME = 'jiayuan-v35'
 const ASSETS = ['./', './index.html', './manifest.json',
                 './icon-192.png', './icon-512.png',
                 './icons/mascot.png', './icons/eat.png', './icons/work.png',
